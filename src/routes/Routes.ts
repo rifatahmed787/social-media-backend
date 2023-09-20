df5ede4eb4d04839cf7fd08408798690979d0a5a
Rifat <takdircoders@gmail.com>
2023-09-20 12:06:33 +0600

    auth added

---
+++ src/routes/Routes.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import { UserRoute } from '../app/modules/user/user.routes'
+import { AuthRoute } from '../app/modules/auth/auth.route'
+import { UploadRoute } from '../app/modules/cloudinary/upload.route'
+import { BookRoute } from '../app/modules/media/book.routes'
+import { ReviewRoute } from '../app/modules/comment/review.route'
+
+import { BlogRoute } from '../app/modules/blog/blog.routes'
+
+const router = express.Router()
+
+const all_routes = [
+  { path: '/auth', router: AuthRoute },
+  { path: '/upload', router: UploadRoute },
+  { path: '/', router: UserRoute },
+  { path: '/users', router: UserRoute },
+  { path: '/books', router: BookRoute },
+  { path: '/reviews', router: ReviewRoute },
+
+  { path: '/blog', router: BlogRoute },
+]
+
+all_routes.map(item => router.use(item.path, item.router))
+
+export default router
